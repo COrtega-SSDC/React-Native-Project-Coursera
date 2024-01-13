34ebdf08cf4fb4dd95accd8d3f5201cf52ff4271
COrtega <carlos.ortega@sjsu.edu>
2024-01-12 17:34:35 -0800

    added email validation and final styling

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,18 +1,32 @@
 import * as React from 'react';
 import { View, StyleSheet, Image, Text, TextInput, Pressable, Alert } from 'react-native';
 
+import { validateEmail } from '../utils';
+
 const SubscribeScreen = () => {
+  const [email, onChangeEmail] = React.useState('')
+  const [isEmailValid, setisEmailValid] = React.useState(false)
+
+  React.useEffect(() => {
+    setisEmailValid(validateEmail(email));
+  }, [email])
 
   return (
     <View styles={styles.container}>
       <Image source={require('../assets/little-lemon-logo-grey.png')} style={styles.logo} />
       <Text style={styles.headerText}>
-          Subscribe to our newsletter for our latest delicious recipes!
+        Subscribe to our newsletter for our latest delicious recipes!
       </Text>
-      <TextInput style={styles.inputBox}>
-
-      </TextInput>
-      <Pressable style={styles.button} onPress={()=> Alert.alert("Thanks for subscribing, stay tuned")}>
+      <TextInput
+        style={styles.inputBox}
+        placeholder='Type your email'
+        keyboardType={'email-address'}
+        value={email}
+        onChangeText={onChangeEmail} />
+      <Pressable
+        style={[styles.button, !isEmailValid && styles.buttonDisabled]}
+        onPress={() => Alert.alert("Thanks for subscribing, stay tuned!")}
+        disabled={!isEmailValid}>
         <Text style={styles.buttonText}>Subscribe</Text>
       </Pressable>
     </View>
@@ -32,29 +46,36 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   logo: {
-    height: 200,
+    height: 125,
     width: 300,
     resizeMode: 'contain',
-    margin: 55
+    alignSelf: "center",
+    marginTop: 30
   },
   button: {
     backgroundColor: '#40534B',
-    margin: 30,
-    fontSize: 50,
-    borderRadius: 16,
+    borderRadius: 10,
+    textAlign: "center",
+    marginLeft: 19,
+    marginRight: 19
   },
   buttonText: {
     textAlign: 'center',
     color: "white",
-    fontSize: 20,
+    fontSize: 17,
     margin: 10
+  },
+  buttonDisabled: {
+    backgroundColor: "#B7B7B7"
   },
   inputBox: {
     height: 40,
-    margin: 12,
+    marginLeft: 20,
+    marginRight: 20,
     borderWidth: 1,
+    borderRadius: 10,
     padding: 10,
     fontSize: 16,
-    backgroundColor: '#EDEFEE',
+    marginBottom: 30
   },
 })
