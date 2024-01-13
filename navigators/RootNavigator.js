a8c816c7f1a1393b114c6f5a219feff7fd04927f
COrtega <carlos.ortega@sjsu.edu>
2024-01-12 16:14:36 -0800

    created the routes for the screen

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import SubscribeScreen from "../screens/SubscribeScreen";
@@ -7,9 +8,11 @@ const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <Stack.Navigator>
-      {/* Set up stack navigation to move between welcome screen and subscribe screen here */}
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ title: 'Welcome', headerTitleAlign: 'center' }}/>
+      <Stack.Screen name="Subscribe" component={SubscribeScreen} options={{ title: 'Subscribe', headerTitleAlign: 'center' }} />
     </Stack.Navigator>
+
   );
 };
 
