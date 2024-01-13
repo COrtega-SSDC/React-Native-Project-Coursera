c2816bf4c411d96dd38bb05ddb18d9234f8ed4a6
COrtega <carlos.ortega@sjsu.edu>
2024-01-12 16:11:55 -0800

    created the Welcome screen with a logo, button,slogan

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,9 +1,55 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
-  // Add welcome screen code here.
-  return <View></View>;
+  return (
+      <View style={styles.container}>
+        <Image source={require('../assets/little-lemon-logo.png')} style={styles.logo} />
+        <Text style={styles.headerText}>
+          Little Lemon, your local Mediterranean Bistro
+        </Text>
+        <Pressable style={styles.button} onPress={() => navigation.navigate('Subscribe')} >
+            <Text style={styles.buttonText}>Newsletter</Text>
+        </Pressable>
+      </View>
+
+  )
+
 };
 
 export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  logo: {
+    height: 225,
+    width: 325,
+    resizeMode: 'contain',
+    alignItems: "center",
+    marginTop: 160,
+    marginBottom: 35
+  },
+  headerText: {
+    padding: 40,
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 150
+  },
+  button: {
+    backgroundColor: '#40534B',
+    fontSize: 50,
+    borderRadius: 10,
+    paddingLeft: 130,
+    paddingRight: 130
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: "white",
+    fontSize: 17,
+    margin: 10
+  }
+})
